d503ff122ff398d7adeddf8455ad64e68bcb98da
Ruminere <brendamccaffery1004@gmail.com>
2024-12-12 01:24:43 -0600

    2024 12 2

--- 2024/12/main.py
+++ 2024/12/main.py
@@ -25,7 +25,7 @@ def main():
             # ans1 += part1(line)
             # ans2 += part2(line)
     grid = ftg(file)
-    ans1 = part1(grid)
+    ans1, ans2 = solve(grid)
 
     print("ENSURE THAT THIS IS NOT THE TEST INPUT")
     print("1:", ans1)
@@ -33,8 +33,9 @@ def main():
 
 # ==================================================
 
-def part1(grid):
-    ans = 0
+def solve(grid):
+    ans1 = 0
+    ans2 = 0
 
     plots = defaultdict(list)
     plots_dupl = defaultdict(int)
@@ -49,10 +50,12 @@ def part1(grid):
     for plot, coords in plots.items():
         area = len(coords)
         perimeter = find_perimeter(grid, coords)
-        print(plot,area,perimeter)
-        ans += area*perimeter
+        edges = find_edges(grid, coords)
+        ans1 += area*perimeter
+        ans2 += area*edges
+        print(plot,area,edges)
     
-    return ans
+    return ans1, ans2
 
 def bfs(grid, start):
     closed = set()
@@ -84,10 +87,40 @@ def find_perimeter(grid, coords):
                 perimeter += 1
     return perimeter
 
-
-
-def part2(line):
-    pass
+def find_edges(grid, coords):
+    edges = 0
+    dirs = [(-1,0),(1,0),(0,-1),(0,1)]
+    edges_raw = {d:[] for d in dirs}
+    for current in coords:
+        for d in dirs:
+            nbr = (current[0]+d[0], current[1]+d[1])
+            if nbr not in coords:
+                edges_raw[d].append(nbr)
+    for d in [(-1,0),(1,0)]:
+        edge_raw = sorted(edges_raw[d])
+        # print(d,edge_raw)
+        for i in rlen1(edge_raw):
+            c0 = edge_raw[i]
+            c1 = edge_raw[i+1]
+            if c0[0] != c1[0] or abs(c0[1]-c1[1]) != 1:
+                edges += 1
+        # print(edges)
+    for d in [(0,-1),(0,1)]:
+        edge_raw = sorted(edges_raw[d])
+        edge_raw.sort(key=lambda i:i[1])
+        print(d,edge_raw)
+        c = 0
+        for i in rlen1(edge_raw):
+            c0 = edge_raw[i]
+            c1 = edge_raw[i+1]
+            if c0[1] != c1[1] or abs(c0[0]-c1[0]) != 1:
+                c += 1
+                edges += 1
+        print(c)
+        
+        # print(edges)
+        
+    return edges+4
 
 # ==================================================
 
