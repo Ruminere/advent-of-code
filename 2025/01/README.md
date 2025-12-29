669b0d6453ac07c271ff7fd047527326c87bc354
Ruminere <brendamccaffery1004@gmail.com>
2025-12-28 22:32:51 -0500

    2024 01 part 1 done

---
+++ 2025/01/README.md
@@ -0,0 +1,5 @@
+# Problem 01
+
+*December 28, 2025*
+
+It has been a year since I last used C, and dang am I rusty.
--- 2025/01/main.c
+++ 2025/01/main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main()
 {
-    char* file_to_open = "test.in";
+    char* file_to_open = "actual.in";
     FILE* input_file;
     char* line = NULL;
     size_t size = 32;
+    char current ;
+
+    char direction;
+    char turns_raw[4];
+    short turns;
+    short dial = 50;
+
+    short counter1 = 0;
 
     input_file = fopen(file_to_open, "r");
     if (input_file == NULL) {
@@ -17,9 +26,27 @@ int main()
 
     // =============== PROCESS EACH LINE ===============
     while (getline(&line, &size, input_file) != -1) {
+        // conversion
         printf("%s", line);
+        direction = line[0];
+        int i;
+        for (i = 1; isdigit(line[i]); i++) {
+            turns_raw[i-1] = line[i];
+        }
+        turns_raw[i-1] = '\0';
+        turns = atoi(turns_raw);
+        if (direction == 'L') turns *= -1;
+        
+        printf("%d\n", turns);
+
+        // logic
+        dial += turns;
+        dial %= 100;
+        printf("after: %d\n", dial);
+        if (!dial) counter1++;
     }
     // =============== PROCESS EACH LINE ===============
 
+    printf("Part 1: %d\n", counter1);
     return 0;
 }
