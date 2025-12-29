ea20a576706b29c91541eb93971e7b20b4c0dcde
Ruminere <brendamccaffery1004@gmail.com>
2025-12-28 22:51:38 -0500

    2025 01 part2 finish

--- 2025/01/main.c
+++ 2025/01/main.c
@@ -17,6 +17,7 @@ int main()
     short dial = 50;
 
     short counter1 = 0;
+    short counter2 = 0;
 
     input_file = fopen(file_to_open, "r");
     if (input_file == NULL) {
@@ -35,11 +36,20 @@ int main()
         }
         turns_raw[i-1] = '\0';
         turns = atoi(turns_raw);
+        counter2 += (int)turns/100;
+        turns %= 100;
         if (direction == 'L') turns *= -1;
         
         printf("%d\n", turns);
 
         // logic
+        if ( (abs(dial) < abs(turns)) && ((dial>0 && turns<0) || (dial<0 && turns>0)) ) {
+            counter2++;
+        }
+        if (abs(dial + turns) > 100) {
+            counter2++;
+        }
+
         dial += turns;
         dial %= 100;
         printf("after: %d\n", dial);
@@ -48,5 +58,6 @@ int main()
     // =============== PROCESS EACH LINE ===============
 
     printf("Part 1: %d\n", counter1);
+    printf("Part 2: %d\n", counter1+counter2);
     return 0;
 }
